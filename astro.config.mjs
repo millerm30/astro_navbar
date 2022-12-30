8241d1cc908b490d53e6b577431b63214b54b7a3
millerm30 <millergm30@gmail.com>
2022-12-30 11:21:58 -0330

    testing again...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://millerm30.github.io',
+  base: '/astro_navbar',
+});
